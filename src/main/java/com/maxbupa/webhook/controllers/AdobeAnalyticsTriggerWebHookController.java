48ae770ac5c8a33ed302ea5e032bf07638eb8f9b
Krishnakanth <krishnakanth.patnaiknaguru@icfolson.com>
2020-09-15 10:32:21 +0530

    validityCheckCommented

--- src/main/java/com/maxbupa/webhook/controllers/AdobeAnalyticsTriggerWebHookController.java
+++ src/main/java/com/maxbupa/webhook/controllers/AdobeAnalyticsTriggerWebHookController.java
@@ -2,13 +2,14 @@ package com.maxbupa.webhook.controllers;
 
 
 import com.maxbupa.webhook.services.AdobeAnalyticsTriggerWebHookService;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/maxbupa/v1")
 public class AdobeAnalyticsTriggerWebHookController {
     private AdobeAnalyticsTriggerWebHookService analyticsTriggerWebHookService;
-
+    private Integer Count = 0;
     public AdobeAnalyticsTriggerWebHookController(AdobeAnalyticsTriggerWebHookService analyticsTriggerWebHookService) {
         this.analyticsTriggerWebHookService = analyticsTriggerWebHookService;
     }
@@ -20,6 +21,9 @@ public class AdobeAnalyticsTriggerWebHookController {
 
     @PostMapping("/trigger/webhook")
     public String insertUpdateTriggerData(@RequestBody String triggerData) {
+        Count += 1;
+        LoggerFactory.getLogger(AdobeAnalyticsTriggerWebHookController.class).info(String.valueOf(Count));
+        //return triggerData;
         return  analyticsTriggerWebHookService.insertUpdateData(triggerData);
     }
 
